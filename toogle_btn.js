83ecf5f31795cd1dd88f001d4a6fcb24d92291d6
XanDamian <kelvinemmanuel2407@gmail.com>
2025-11-14 20:17:21 +0100

    added tooglt_btn.js

---
+++ toogle_btn.js
@@ -0,0 +1,9 @@
+const toogle_btn = document.querySelector('.toogle_btn');
+const toogle_btn_icon = document.querySelector('.toogle_btn i');
+const mobile_menu = document.querySelector('.mobile_menu');
+
+toogle_btn.unclick = function() {
+    mobile_menu.classList.toggle('open');
+    const isopen = mobile_menu.classList.contains('open');
+    toogle_btn_icon.classList = isopen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+}
